2ce7c8bd84c953c6c2cadace83be795d706fa620
Tomasz Kordiak <dotomika@gmail.com>
2026-05-19 22:42:35 +0200

    initial commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+// Get root elements
+const html = document.querySelector("html");
+const head = document.querySelector("head");
+const body = document.querySelector("body");
+if (!html || !head || !body) throw new Error("index.html has invalid structure");
+
+// Prepare `title` element
+const title = document.querySelector("title") || document.createElement("title");
+if (!title.parentNode) head.append(title);
+
+// Set main attributes
+title.text = "App";
+html.lang = "en"
+html.dir = "ltr"
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [],
+  server: {
+    host: "0.0.0.0",
+    port: 3002,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 3002,
+  },
+});
