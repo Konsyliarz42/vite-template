29e83239e4541287ceac635c5036164eca57fee5
Tomasz Kordiak <dotomika@gmail.com>
2026-09-12 18:14:35 +0200

    chore: update biome

--- vite.config.ts
+++ vite.config.ts
@@ -2,20 +2,20 @@ import path from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [],
   clearScreen: false,
+  plugins: [],
+  preview: {
+    host: "0.0.0.0",
+    port: 3002,
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@public": path.resolve(__dirname, "./public"),
+      "@": path.resolve(import.meta.dirname, "./src"),
+      "@public": path.resolve(import.meta.dirname, "./public"),
     },
   },
   server: {
     host: "localhost",
-    port: 3002,
-  },
-  preview: {
-    host: "0.0.0.0",
     port: 3002,
   },
 });
